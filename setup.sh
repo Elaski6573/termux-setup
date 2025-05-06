53f504a614e98603d10e0146fb6a98984ecc6243
Elaski <114150904+Elaski6573@users.noreply.github.com>
2025-05-06 09:50:47 +0900

    Update setup.sh
    필수 패키지 설치 과정 추가

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # 패키지 업데이트 & 패키지 설치
+echo "패키지를 업데이트합니다.."
 if pkg update && pkg upgrade -y; then
     echo "업데이트 완료"
 else
@@ -10,4 +11,16 @@ else
     else
         echo "오류가 발생했습니다. 자세한 사항은 로그를 확인하세요"
         exit 1
+    fi
+fi
+
+echo "1. 필수 패키지 설치하기"
+echo "2. 끝내기"
+
+if ["$choice" = "1"] ;then
+    echo "필수 패키지들을 설치합니다"
+    pkg install wget vim git -y
+else
+    echo "종료합니다"
+    exit 1
 fi
